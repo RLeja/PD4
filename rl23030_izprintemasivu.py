cdb6989a18f587d879831fe0c40edd5b2f522fa3
RLeja <testaprofils132@inbox.lv>
2024-12-15 16:33:08 +0200

    "geoprintmasivs" added

--- rl23030_izprintemasivu.py
+++ rl23030_izprintemasivu.py
@@ -1,3 +1,11 @@
 def printmasivs(masivs):
     for elements in masivs:
         print(elements)
+
+def geoprintmasivs(masivs):
+    if(len(masivs)<=0): return 
+    prev = masivs[0]
+    print(prev)
+    for i in range(1,len(masivs)):
+        prev+=masivs[i]
+        print(prev)
